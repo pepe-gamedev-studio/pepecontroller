e36bc00e2119567d871c2fe408e8aded5b7cfea6
JudgeGC <judgegc@gmail.com>
2021-03-10 20:52:34 +0200

    Case insensitive db schema

--- pepecontroller/src/storage/models/user.cpp
+++ pepecontroller/src/storage/models/user.cpp
@@ -9,7 +9,7 @@ TableType MakeTable()
 
 	return make_table("user",
 		make_column("id", &User::id, primary_key()),
-		make_column("name", &User::name),
+		make_column("name", &User::name, collate_nocase()),
 		make_column("vote_weight", &User::voteWeight),
 		make_column("group", &User::group));
 }
